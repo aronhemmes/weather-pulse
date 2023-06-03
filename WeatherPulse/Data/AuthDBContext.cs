8a5bdb3f8886d85df131cbc43c1402df85011200
Aron Hemmes <aronhemmes@gmail.com>
2023-06-03 11:43:34 +0200

    Add database

---
+++ WeatherPulse/Data/AuthDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherPulse.Models;
+
+namespace WeatherPulse.Data
+{
+    public class AuthDBContext : DbContext
+    {
+        public AuthDBContext(DbContextOptions<AuthDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; } = default!;
+
+    }
+}
