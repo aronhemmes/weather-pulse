9b2b74016a0b6ca229adb65920e9235b3a62e08d
Aron Hemmes <aronhemmes@gmail.com>
2023-06-03 11:43:38 +0200

    Add user

---
+++ WeatherPulse/Models/RegisterUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherPulse.Models
+{
+    [Index(nameof(Phone), IsUnique = true)]
+    public class RegisterUser
+    {
+        [Required, Phone]
+        public string Phone { get; set; }
+        [Required]
+        //[RegularExpression("YourRegularExpressionPattern", ErrorMessage = "Invalid location format")]
+        public string Location { get; set; }
+    }
+}
---
+++ WeatherPulse/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherPulse.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Phone { get; set; }
+        public string Location { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public string Timezone { get; set; }
+    }
+}
